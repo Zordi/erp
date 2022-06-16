32e915a610268a0c899b8346da4dbe2fb7115109
zordi <zordi.smakom@gmail.com>
2022-06-16 14:26:25 +0700

    first commit

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,24 @@
+<head><base href="../">
+    <title>ERP For You</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="ERP untuk pengelolaan perusahaan yang lebih baik." />
+    <meta name="keywords" content="ERP Teknodigital, erp, erp finance, erp perusahaan" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:locale" content="id_ID" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Teknodigital ERP" />
+    <meta property="og:url" content="https://teknodigital.id" />
+    <meta property="og:site_name" content="ERP | Teknodigital" />
+    <link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.ico') }}" />
+    <!--begin::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <!--end::Fonts-->
+    <!--begin::Page Vendor Stylesheets-->
+    <link href="{{ asset('assets/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/plugins/custom/vis-timeline/vis-timeline.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <!--end::Page Vendor Stylesheets-->
+    <!--begin::Global Stylesheets Bundle-->
+    <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <!--end::Global Stylesheets Bundle-->
+</head>
